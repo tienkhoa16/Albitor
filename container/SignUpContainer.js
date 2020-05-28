e17f648a5c6c3321d4b7c0f582f422c0b3abc29c
tienkhoa16 <nguyentienkhoa22@gmail.com>
2020-05-29 01:56:21 +0800

    Obtain name of user

--- container/SignUpContainer.js
+++ container/SignUpContainer.js
@@ -9,13 +9,17 @@ axios.defaults.withCredentials = true;
 const instance = axios.create();
 instance.defaults.timeout = 20000;
 
-async function authAndGetCookie(username, password){
+async function auth(username, password){
     console.log("[AUTH]")
+
+    let resp = {
+        headers: {},
+    }
 
     // Basic check first
     if (username[0] != 'E' && username[0] != 'e' || username.length != 8)
-        return false
-        
+        return resp
+    
     // Log out url
     const log_out_url =  'https://vafs.nus.edu.sg/adfs/ls/?wa=wsignout1.0'
 
@@ -45,10 +49,23 @@ async function authAndGetCookie(username, password){
     // Log out first if there were any successful sign in earlier
     const log_out = await axios.get(log_out_url)
 
-    const resp = await axios.post(auth_url, data, config)
+    resp = await axios.post(auth_url, data, config)
         .then((response) => response)
         .catch((err) => err.response)
-    return resp.headers['set-cookie']
+    return resp
+}
+
+function getName(data){
+    // Start position of name
+    let startPos = data.indexOf('Login User') + 18
+
+    let name = ''
+    while(data[startPos] != '&'){
+        name += data[startPos]
+        startPos++
+    }
+
+    return name
 }
 
 export default class SignUpContainer extends React.Component{
@@ -59,6 +76,7 @@ export default class SignUpContainer extends React.Component{
         typing: false,
         authenticating: false,
         signInSuccesful: false,
+        name: '',
     };
 
     handleUpdateUsername = (username, typing) => this.setState({username, typing: true, firstTime: false});
@@ -66,7 +84,7 @@ export default class SignUpContainer extends React.Component{
     handleUpdatePassword = (password, typing) => this.setState({password, typing: true, firstTime: false});
 
     render(){
-        const {username, password, firstTime, typing, authenticating, signInSuccesful} = this.state
+        const {username, password, firstTime, typing, authenticating, signInSuccesful, name} = this.state
 
         return(
             <KeyboardAvoidingView style = {styles.container}>
@@ -93,8 +111,14 @@ export default class SignUpContainer extends React.Component{
                         ){
                             (async () => {
                                 this.setState({authenticating:true})
-                                const code_url = await authAndGetCookie(username, password)
-                                code_url ? (this.setState({signInSuccesful:true, authenticating:false})):(this.setState({signInSuccesful:false, authenticating:false}))
+                                const resp = await auth(username, password)
+
+                                if ('set-cookie' in resp.headers){
+                                    this.setState({signInSuccesful:true, authenticating:false, name: getName(resp.data)})
+                                }
+                                else{
+                                    this.setState({signInSuccesful:false, authenticating:false})
+                                }
                             })()
                             this.setState({
                                 username: '',
@@ -108,6 +132,9 @@ export default class SignUpContainer extends React.Component{
                 </BlueButton>
                 {
                     (firstTime || typing)? null : ((authenticating)? (<Text style = {styles.text}>Authenticating...</Text>)  : (signInSuccesful ? (<Text style = {styles.text}>Log In Successful</Text>) : (<Text style = {styles.err_text}>Wrong NUSNET or Password</Text>)))
+                }
+                {
+                    (name) ? (<Text style = {styles.text}>Welcome {name}</Text>) : null
                 }
             </KeyboardAvoidingView>
         );
